a62fec71561517e97c22e8587b99ffff1a877f5a
schomme <schomme@gmx.net>
2022-10-18 14:47:54 +0200

    Correction of Length formular

--- VectorLib/Vector.cs
+++ VectorLib/Vector.cs
@@ -7,7 +7,7 @@
         public double Y { get; set; }
         public double Z { get; set; }
 
-        public double Length => Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Y * this.Y);
+        public double Length => Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
 
         public Vector(double x, double y, double z)
         {
