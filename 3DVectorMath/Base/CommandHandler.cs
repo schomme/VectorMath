a249877d2b747d334847d003978d2802c167642e
schomme <schomme@gmx.net>
2022-10-19 14:20:31 +0200

    Added Commandhandler

---
+++ 3DVectorMath/Base/CommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+using VectorLib;
+
+namespace _3DVectorMath.Base
+{
+    public class CommandHandler : ICommand
+    {
+        private Action<Vector> _action;
+        private bool _canExecute;
+        public CommandHandler(Action<Vector> action, bool canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action(parameter as Vector);
+        }
+    }
+}
